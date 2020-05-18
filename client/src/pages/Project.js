985a6548f290c4fc9a8bc2ed427ad092c1b72947
Carlos <clockwerkz@gmail.com>
2020-05-18 09:24:49 -0700

    adding editable state to project page for projectAdmins

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //Project ID, Name, Description, githubIdentifier, 
 //Project Stus, Location(s), Created Date, Completed Date, 
@@ -7,12 +7,24 @@ import React from 'react';
 //LFM - A way to tag roles that a current project is looking to fill? Tags?
 
 const Project = (props) => {
-
-    return (
-        <div>
-            <h1>Project Info Page</h1>
-        </div>
-    )
+    const [ isEditing, setIsEditing ] = useState(false);
+    if (isEditing) {
+        return (
+            <div>
+                <h1>Project Info Page</h1>
+                <label htmlFor="projectName">Project Name:</label>
+                <input name="projectName" type="text" value="Name"></input>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>Project Info Page</h1>
+                <p>Project Name:</p>
+                <p>Project Name</p>
+            </div>
+        )
+    }
 }
 
 
