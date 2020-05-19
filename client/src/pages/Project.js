897352352e349b98049ef717e7266052276ff3bb
Carlos <clockwerkz@gmail.com>
2020-05-18 20:22:39 -0700

    wrapping up first pass

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../sass/Project.scss';
 
-//LFM - A way to tag roles that a current project is looking to fill? Tags?
-//Project ID, Name, Description, githubIdentifier, 
-//Project Stus, Location(s), Created Date, Completed Date, 
-//Github URL, Slack URL, googleDriveURL, Videoconference Link
-//lookingDescription, recruitingCategories, partners
+
 
 const Project = (props) => {
     const [ isEditing, setIsEditing ] = useState(false);
@@ -27,6 +23,8 @@ const Project = (props) => {
             [name] : value
         })
     }
+    const createdDate = data.createdDate ? data.createdDate.split('T')[0] : '';
+    const completedDate = data.completedDate ? data.completedDate.split('T')[0] : 'In Development';
     if (isEditing) {
 
         return (
@@ -37,20 +35,20 @@ const Project = (props) => {
                 </div>
                 <div className="project-entry">
                     <label htmlFor="name" className="project-entry__label">Project Name:</label>
-                    <input name="name" type="text" className="project-name__data"
+                    <input name="name" type="text" className="project-entry__input"
                     value={data.name ? data.name : ''} 
                     onChange={(e)=>handleInputChange("name", e.target.value)}></input>
                 </div>
                 <div className="project-entry">
                     <label htmlFor="description" className="project-entry__label">Description:</label>
-                    <textarea name="description" type="text" className="project-name__data" rows="5" cols="40"
+                    <textarea name="description" type="text" className="project-entry__input-text" rows="5" cols="40"
                         onChange={(e)=>handleInputChange("description", e.target.value)}>
                             {data.description ?  data.description : ''}
                         </textarea>
                 </div> 
                 <div className="project-entry">
                     <label htmlFor="githubIdentifier" className="project-entry__label">Github Identifier:</label>
-                    <input name="githubIdentifier" type="text" className="project-name__data"
+                    <input name="githubIdentifier" type="text" className="project-entry__input"
                         value={data.githubIdentifier ?  data.githubIdentifier : ''}
                         onChange={(e)=>handleInputChange("githubIdentifier", e.target.value)}></input>
                 </div> 
@@ -59,41 +57,54 @@ const Project = (props) => {
                     <p class="project-entry__data">Active</p>
                 </div> 
                 <div className="project-entry">
-                    <p class="project-entry__label">Location(s):</p>
-                    <p class="project-entry__data">Los Angles, CA</p>
-                </div> 
-                <div className="project-entry">
-                    <p class="project-entry__label">Created:</p>
-                    <p class="project-entry__data">March 2020</p>
-                </div> 
-                <div className="project-entry">
-                    <p class="project-entry__label">Completed:</p>
-                    <p class="project-entry__data">In-Prog</p>
-                </div> 
-                <div className="project-entry">
-                    <p class="project-entry__label">Github URL:</p>
-                    <p class="project-entry__data">Github</p>
-                </div> 
-                <div className="project-entry">
-                    <p class="project-entry__label">Slack URL:</p>
-                    <p class="project-entry__data">Slack</p>
-                </div> 
-                <div className="project-entry">
-                    <p class="project-entry__label">google drive URL:</p>
-                    <p class="project-entry__data">Google Drive</p>
-                </div> 
-                <div className="project-entry">
-                    <p class="project-entry__label">VideoConference:</p>
-                    <p class="project-entry__data">video conference link</p>
-                </div> 
-                <div className="project-entry">
-                    <p class="project-entry__label">lookingDescription:</p>
-                    <p class="project-entry__data">I'm baby kale chips etsy sustainable pok pok. Franzen waistcoat jean shorts, craft beer biodiesel messenger bag.</p>
-                </div> 
-                <div className="project-entry">
-                    <p class="project-entry__label">recruiting categories:</p>
-                    <p class="project-entry__data">Front End, Back End, Full Stack, React</p>
+                    <label htmlFor="location" className="project-entry__label">Location(s):</label>
+                    <input name="location" type="text" className="project-entry__data"
+                    value={data.location ? data.location : ''} 
+                    onChange={(e)=>handleInputChange("location", e.target.value)}></input>
                 </div>
+                <div className="project-entry">
+                    <label htmlFor="createdDate" className="project-entry__label">Created:</label>
+                    <input name="createdDate" type="date" className="project-entry__data"
+                    value={createdDate} 
+                    onChange={(e)=>handleInputChange("createdDate", e.target.value)}></input>
+                </div>
+                <div className="project-entry">
+                    <label htmlFor="completedDate" className="project-entry__label">Created:</label>
+                    <input name="completedDate" type="date" className="project-entry__data"
+                    value={completedDate} 
+                    onChange={(e)=>handleInputChange("completedDate", e.target.value)}></input>
+                </div>
+                <div className="project-entry">
+                    <label htmlFor="githubUrl" className="project-entry__label">Github URL:</label>
+                    <input name="githubUrl" type="text" className="project-entry__data"
+                    value={data.githubUrl ?  data.githubUrl : ''}
+                    onChange={(e)=>handleInputChange("githubUrl", e.target.value)}></input>
+                </div> 
+                <div className="project-entry">
+                    <label htmlFor="slackUrl" className="project-entry__label">Slack URL:</label>
+                    <input name="slackUrl" type="text" className="project-entry__data"
+                    value={data.slackUrl ?  data.slackUrl : ''}
+                    onChange={(e)=>handleInputChange("slackUrl", e.target.value)}></input>
+                </div> 
+                <div className="project-entry">
+                    <label htmlFor="googleDriveUrl" className="project-entry__label">Google Drive URL:</label>
+                    <input name="googleDriveUrl" type="text" className="project-entry__data"
+                    value={data.googleDriveUrl ?  data.googleDriveUrl : ''}
+                    onChange={(e)=>handleInputChange("googleDriveUrl", e.target.value)}></input>
+                </div> 
+                <div className="project-entry">
+                    <label htmlFor="videoConferenceLink" className="project-entry__label">Zoom Link:</label>
+                    <input name="videoConferenceLink" type="text" className="project-entry__data"
+                    value={data.videoConferenceLink ?  data.videoConferenceLink: ''}
+                    onChange={(e)=>handleInputChange("videoConferenceLink", e.target.value)}></input>
+                </div> 
+                <div className="project-entry">
+                    <label htmlFor="description" className="project-entry__label">Recruiting Categories:</label>
+                    <textarea name="description" type="text" className="project-entry__input-text" rows="5" cols="40"
+                        onChange={(e)=>handleInputChange("lookingDescription", e.target.value)}>
+                            {data.lookingDescription ?  data.lookingDescription : ''}
+                        </textarea>
+                </div> 
                 <div className="project-entry">
                     <p class="project-entry__label">Partners:</p>
                     <p class="project-entry__data">partners</p>
@@ -129,7 +140,7 @@ const Project = (props) => {
                 </div> 
                 <div className="project-entry">
                     <p class="project-entry__label">Created:</p>
-                    <p class="project-entry__data">{data.createdDate ?  data.createdDate : ''}</p>
+                    <p class="project-entry__data">{createdDate}</p>
                 </div> 
                 <div className="project-entry">
                     <p class="project-entry__label">Completed:</p>
